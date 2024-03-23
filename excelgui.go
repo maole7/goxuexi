ef71acc1d90b69bda71d8c36dbd4b7944644d5b2
maole7 <qiwang99@126.com>
2024-03-23 20:19:06 +0800

    init

--- excelgui.go
+++ excelgui.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"fmt"
-
+  "log"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello, World!")
 	questionButton := widget.NewButton("Ask a Question", func() {
-		fmt.Println("You clicked the button!")
+		f, err := excelize.OpenFile("D:/ymk.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 读取指定工作表的数据
+	rows, err := f.GetRows("Sheet1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var brr, grr, hrr, irr []string
+
+	for _, row := range rows {
+		brr = append(brr, row[1]) // B列数据
+		grr = append(grr, row[6]) // G列数据
+		hrr = append(hrr, row[7]) // H列数据
+		irr = append(irr, row[8]) // I列数据
+	}
+
+	// 输出读取的数据
+	fmt.Println("B 列数据:", brr)
+	fmt.Println("G 列数据:", grr)
+	fmt.Println("H 列数据:", hrr)
+	fmt.Println("I 列数据:", irr)
 	})
   content := container.NewVBox(
 		questionButton,
